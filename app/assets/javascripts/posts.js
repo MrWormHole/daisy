869c05c35e525ac1381f483e9dd7d0ce0b24e13f
MrWormHole <talhaaltinel@hotmail.com>
2019-08-30 23:23:01 +0300

    view changes

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,10 @@
-document.addEventListener('dnd-upload:error', function (event) {
-    event.preventDefault()
-})
+$(document).ready(function() {
+    console.log("LOADED Jquery");
+    $(".tester").click(function () {
+        $(this).replaceWith("<h1>JQuery is Great</h1>");
+    });
+
+    $(".photoButton").click(function() {
+       $(".foo").click();
+    });
+});
